88523d686f2827c1ea58949451cb31d40da657bc
Dmitry Lyzin <dmitry_lyzin@mail.ru>
2021-10-11 23:46:08 +0300

    Переписал Tone{} под BCDnum< uint16_t, ...>

--- Tone.cpp
+++ Tone.cpp
@@ -1,32 +1,29 @@
 #include "Tone.h"
 
-#define digital_tone_bit b10000000
-#define digital_tone_polarity_bit b01000000
+#define digital_tone_bit		(b10000000 << 8)
+#define digital_tone_polarity_bit	(b01000000 << 8)
 
 //----------------------------------------------------------------
 void Tone::read( std::istream& input )
 {
 	if( input >>= "OFF" )
 	{
-		b[0] = 0xFFu;
-		b[1] = 0xFFu;
+		u.bin = 0xFFFFu;
 		return;
 	}
 
 	if( input >>= 'D' )
 	{
-		b[1]  = getdigit( input );
-		b[1] |= digital_tone_bit;
-		b[0]  = getdigit( input ) << 4;
-		b[0] |= getdigit( input );
+		input >> u.digital_tone;
 
+		u.bin |= digital_tone_bit;
 		switch( input.get() )
 		{
 		case 'I':
-			b[1] |= digital_tone_polarity_bit;
+			u.bin |= digital_tone_polarity_bit;
 			break;
 		case 'N':
-			b[1] &= ~digital_tone_polarity_bit;
+			u.bin &= ~digital_tone_polarity_bit;
 			break;
 		default:
 			input.unget();
@@ -36,54 +33,22 @@ void Tone::read( std::istream& input )
 		return;
 	}
 
-	char buf[8] = { 0 };
-	char* p = buf + 4;
-
-	*p++ = getdigit( input );
-
-	char c;
-	while( input.get( c ) )
-	{
-		if( !isdigit( c ) )
-		{
-			input.unget();
-			break;
-		}
-		if( p < buf + std::size( buf ) )
-			*p++ = c - '0';
-	}
-
-	input >> '.';
-
-	b[0]  = getdigit( input );
-	b[0] |= (*--p) << 4;
-	b[1]  = (*--p);
-	b[1] |= (*--p) << 4;
+	input >> u.analog_tone;
 }
 
 //----------------------------------------------------------------
 void Tone::print( std::ostream& output ) const
 {
-	if( 0xFFu == b[1] && 0xFFu == b[0] )
+	if( 0xFFFFu == u.bin )
 	{
 		output << "OFF";
 		return;
 	}
-	if( digital_tone_bit & b[1] )
+	if( digital_tone_bit & u.bin )
 	{
-		output << "D"
-			<< bcd1( b[1] )
-			<< bcd2( b[0] ) << bcd1( b[0] )
-			<< (b[1] & digital_tone_polarity_bit ? 'I' : 'N')
-			;
+		output	<< "D" << u.digital_tone
+			<< (u.bin & digital_tone_polarity_bit ? 'I' : 'N');
 		return;
 	}
-
-	char c = bcd2( b[1] );
-	if( '0' != c )
-		output << c;
-
-	output
-		<< bcd1( b[1] )
-		<< bcd2( b[0] ) << '.' << bcd1( b[0] );
+	output << u.analog_tone;
 }
--- Tone.h
+++ Tone.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "common.h"
+#include "BCDnum.h"
+
+static constexpr const char  analog_tone_fmt[] = "-4.10";
+static constexpr const char digital_tone_fmt[] = "03.00";
 
 //----------------------------------------------------------------
 class Tone
 {
-	uint8_t b[2];
+	union
+	{
+		uint16_t bin;
+		BCDnum< uint16_t,  analog_tone_fmt> analog_tone;
+		BCDnum< uint16_t, digital_tone_fmt> digital_tone;
+	} u;
 public:
 	SAMPLE_CLASS
 ( Tone )
