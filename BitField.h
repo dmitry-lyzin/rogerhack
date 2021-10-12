e4b2f94d1563295c5a2edc759889e27cd8d79eda
Dmitry Lyzin <dmitry_lyzin@mail.ru>
2021-10-13 00:38:52 +0300

    BitField{} провратился шаблон BitField<>{}

--- BitField.h
+++ BitField.h
@@ -1,43 +1,59 @@
 #pragma once
 #include "common.h"
+#include "assert.h"
 
-//----------------------------------------------------------------
-class BitField
+#define TEMPL template		\
+< typename T			\
+, const char* state_names[]	\
+, const size_t state_names_len	\
+, const size_t bitindex		\
+>
+#define THIS BitField< T, state_names, state_names_len, bitindex>
+// BitField< T, state_names, state_names_len, bitindex>
+// state_names_len должен == степени двойки
+TEMPL class BitField
 {
-	union
-	{
-		uint8_t *pointer;
-		uint8_t constant;
-	} operand;
-
-	const char** state_names;
-	const size_t state_names_len;
-	const size_t bitindex;
-	const uint8_t bitmask;
-
+protected:
+	T value;
+private:
+	static constexpr const T bitmask = (state_names_len - 1) << bitindex;
 public:
-	BitField( const char* state_names_[], size_t state_names_len_, unsigned int bitindex_ )
-		: operand( { NULL } ), state_names( state_names_ )
-		, state_names_len( state_names_len_ ), bitindex( bitindex_ )
-		, bitmask( (state_names_len_ - 1u) << bitindex_ )
-	{
-		assert	(  b00000010 == state_names_len_ 
-			|| b00000100 == state_names_len_
-			|| b00001000 == state_names_len_
-			|| b00010000 == state_names_len_
-			|| b00100000 == state_names_len_
-			);
-	};
+	void read ( std::istream& input  );
+	void print( std::ostream& output ) const;
+};
+TEMPL inline std::istream& operator>>( std::istream& input,        THIS& s ) { s.read ( input ); return input;  }
+TEMPL inline std::ostream& operator<<( std::ostream& output, const THIS& s ) { s.print( output); return output; }
 
-	BitField& operator()(       uint8_t *operand_) { operand.pointer  = operand_; return *this; };
-	BitField& operator()( const uint8_t  operand_) { operand.constant = operand_; return *this; };
-
-	SAMPLE_CLASS
-( BitField )
-
-
-//----------------------------------------------------------------
-inline void BitField::print( std::ostream& output ) const
+TEMPL        void THIS::read ( std::istream& input  )
 {
-	output << state_names[ (operand.constant & bitmask) >> bitindex];
+	assert(("state_names_len должен == степени двойки", !(state_names_len & (state_names_len - 1)) ));
+
+	for( size_t i = 0; i < state_names_len; i++ )
+	{
+		if( !state_names[i] )
+			continue;
+		if( input >>= state_names[i] )
+		{
+			value &= ~bitmask;
+			value |= T(i) << bitindex;
+			return;
+		}
+	}
+
+	const char* t = "Wrong read! Wait words: \"";
+	for( size_t i = 0; i < state_names_len; i++ )
+	{
+		if( !state_names[i] )
+			continue;
+		cerr << t << state_names[i] << '"';
+		t = " or \"";
+	}
+	streamerror( input );
 }
+TEMPL inline void THIS::print( std::ostream& output ) const
+{
+	output << state_names[(value & bitmask) >> bitindex];
+};
+
+#undef TEMPL
+#undef THIS
--- Flags.h
+++ Flags.h
@@ -1,8 +1,20 @@
 #pragma once
 #include "common.h"
+#include "BitField.h"
+
+static const char* busy2str [] = { "OFF",	"QT/DQT", "Carrier",	NULL	};
+static const char* scan2str [] = { "Delete",	"Add"	};
+static const char* power2str[] = { "Low",	"High"	};
+#define PS(x) x, std::size(x)
+
 class Flags
 {
-	uint8_t byte;
+	union
+	{
+		BitField< uint8_t, PS( busy2str ), 0> busy;
+		BitField< uint8_t, PS( scan2str ), 3> scan;
+		BitField< uint8_t, PS( power2str), 4> power;
+	} u;
 public:
 	SAMPLE_CLASS
-( Flags)
+( Flags )
